a08809677e4edf8fda2e31701ab9c4efcfbeb426
Username <maximorechov@yandex.ru>
2022-04-05 23:38:53 +0500

    "refactor: убрать поле логической связи с эпиком"

--- src/Subtask.java
+++ src/Subtask.java
@@ -1,7 +1,6 @@
 import java.util.Objects;
 
 public class Subtask extends Task {
-    private boolean isEpic = false;
     private int connectionWithEpic;
 
     public Subtask(String name, String description, int identificationNumber, Enum status, int connectionWithEpic) {
@@ -17,23 +16,14 @@ public class Subtask extends Task {
         this.connectionWithEpic = connectionWithEpic;
     }
 
-    public boolean isEpic() {
-        return isEpic;
-    }
-
-    public void setEpic(boolean epic) {
-        isEpic = epic;
-    }
-
     @Override
     public String toString() {
         return "Subtask{" +
-                "isEpic=" + isEpic +
-                ", connectionWithEpic=" + connectionWithEpic +
+                "connectionWithEpic=" + connectionWithEpic +
                 ", name='" + name + '\'' +
                 ", description='" + description + '\'' +
                 ", identificationNumber=" + identificationNumber +
-                ", status='" + status + '\'' +
+                ", status=" + status +
                 '}';
     }
 
@@ -43,11 +33,11 @@ public class Subtask extends Task {
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Subtask subtask = (Subtask) o;
-        return isEpic == subtask.isEpic && connectionWithEpic == subtask.connectionWithEpic;
+        return connectionWithEpic == subtask.connectionWithEpic;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), isEpic, connectionWithEpic);
+        return Objects.hash(super.hashCode(), connectionWithEpic);
     }
 }
