01edb6e6c64cb1c6ea49355e83180c540962b152
Username <maximorechov@yandex.ru>
2022-04-05 20:12:34 +0500

    "feat: оставить создание объекта Managers"

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        TaskManager taskManager = new Managers().getDefault();
+        TaskManager taskManager = Managers.getDefault();
 
-
-        InMemoryTaskManager manager = (InMemoryTaskManager) taskManager;
-
-        manager.setId();
-        Task task = new Task("name", "DFSKF", manager.getId(), Status.DONE);
-        manager.setId();
-        Task task1 = new Task("nam", "DFSKF", manager.getId(), Status.IN_PROGRESS);
-        manager.setId();
-        Task task2 = new Task("na", "DFSKF", manager.getId(), Status.NEW);
-
-        manager.listTask.put(task.identificationNumber, task);
-        manager.listTask.put(task1.identificationNumber, task1);
-        manager.listTask.put(task2.identificationNumber, task2);
-        manager.setId();
-
-        Epic epic = new Epic("12", "sfdfs", manager.getId(), Status.NEW);
-
-        manager.listEpic.put(manager.getId(), epic);
-
-        manager.mainMenu(manager);
+        taskManager.mainMenu(taskManager);
 
     }
 }
