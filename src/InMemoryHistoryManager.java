c36b08a8dd5028dad02f40976a8d417474f2e9dc
Username <maximorechov@yandex.ru>
2022-04-05 23:39:31 +0500

    "refactor: привести в соответсвие с ТЗ заполнение задач"

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -5,19 +5,10 @@ public class InMemoryHistoryManager implements HistoryManager {
     private final List<Task> history = new ArrayList<>();
 
     public void add(Task task) {
-        if (history.size() == 0) {
-            history.add(task);
-        } else {
-            int count = 0;
-            for (Task tasks : history) {
-                if (task.equals(tasks)) {
-                    count++;
-                }
-            }
-            if (count == 0) {
-                history.add(task);
-            }
+        if (history.size() == 10) {
+            history.remove(0);
         }
+        history.add(task);
     }
 
     public List<Task> getHistory() {
