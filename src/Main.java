5417831d24d0858e186407467514ef0204a2e818
Username <maximorechov@yandex.ru>
2022-04-07 19:55:27 +0500

    "refactor: поменять сигнатуру метода. "

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         TaskManager taskManager = Managers.getDefault();
-        taskManager.mainMenu(taskManager);
+        taskManager.mainMenu();
     }
 }
